7e744d0f6c4e35e5594901f99acbfd7821a950b7
Nick Cipollo <nick.cipollo@thelevelup.com>
2019-07-16 17:11:28 -0400

    Fixes #3 Create ApiFactory

---
+++ app/src/main/java/org/mtg/api/ApiFactory.kt
@@ -0,0 +1,42 @@
+package org.mtg.api
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import io.reactivex.schedulers.Schedulers
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+
+class ApiFactory(private val cacheDirectory: String) {
+    companion object {
+        const val BASE_URL = "https://whispering-plateau-91662.herokuapp.com/api"
+        const val CACHE_SIZE = 10 * 1024 * 1024
+        const val TIMEOUT_SECONDS = 30
+    }
+    private val retrofit = createRetrofit()
+
+    inline fun <reified T> createApi() = createApiFromClass(T::class.java)
+
+    fun <T> createApiFromClass(service: Class<T>): T = retrofit.create(service)
+
+    private fun createRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(createGson()))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+
+
+    private fun createGson() =
+        GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+
+    private fun createOkhttp(): OkHttpClient {
+        val cacheFile = File(cacheDirectory, "httpCache")
+        val cache = Cache(cacheFile, CACHE_SIZE.toLong())
+        return OkHttpClient.Builder()
+            .cache(cache)
+            .build()
+    }
+}
