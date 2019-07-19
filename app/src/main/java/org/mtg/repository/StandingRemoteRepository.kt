c7eff4f1a197423477d6f69281428fe2d3efa7b9
Nick Cipollo <nick.cipollo@thelevelup.com>
2019-07-19 16:53:03 -0400

    Add Standing Repository error handling and tests

--- app/src/main/java/org/mtg/repository/StandingRemoteRepository.kt
+++ app/src/main/java/org/mtg/repository/StandingRemoteRepository.kt
@@ -1,7 +1,10 @@
 package org.mtg.repository
 
 import org.mtg.api.StandingApi
+import org.mtg.api.onApiErrorReturn
 
 class StandingRemoteRepository(private val standingApi: StandingApi) {
-    fun standingsForLeague(leagueId: Long) = standingApi.standingsForLeague(leagueId)
+    fun standingsForLeague(leagueId: Long) =
+        standingApi.standingsForLeague(leagueId)
+            .onApiErrorReturn { emptyList() }
 }
---
+++ app/src/test/java/org/mtg/repository/StandingRemoteRepositoryTest.kt
@@ -0,0 +1,51 @@
+package org.mtg.repository
+
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import io.reactivex.subjects.PublishSubject
+import org.junit.Test
+import org.mtg.api.StandingApi
+import org.mtg.model.Standing
+import java.io.IOException
+
+class StandingRemoteRepositoryTest {
+    private companion object {
+        const val LEAGUE_ID = 1L
+
+        val ERROR = IOException()
+        val STANDING = Standing(
+            firstName = "Nick",
+            lastName = "Cipollo",
+            ranking = "1st",
+            week1Record = "3-0",
+            week2Record = "3-0",
+            week3Record = "3-0",
+            week4Record = "3-0"
+        )
+    }
+
+    private val standingSubject = PublishSubject.create<List<Standing>>()
+
+    private val api = mock<StandingApi> {
+        on { standingsForLeague(LEAGUE_ID) } doReturn standingSubject.firstOrError()
+    }
+    private val repo = StandingRemoteRepository(api)
+
+    @Test
+    fun standing_error() {
+        val observer = repo.standingsForLeague(LEAGUE_ID).test()
+        respondWithStandingError()
+        observer.assertValue(emptyList())
+    }
+
+    @Test
+    fun standing_success() {
+        val observer = repo.standingsForLeague(LEAGUE_ID).test()
+        respondWithStandings()
+        observer.assertValue(listOf(STANDING))
+    }
+
+    private fun respondWithStandings() = standingSubject.onNext(listOf(STANDING))
+
+    private fun respondWithStandingError() = standingSubject.onError(ERROR)
+}
