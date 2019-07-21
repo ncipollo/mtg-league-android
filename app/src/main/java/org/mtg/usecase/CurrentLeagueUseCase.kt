e4256222a6837e487528775a49c1a66d7fbdb786
Nick Cipollo <nick.cipollo@thelevelup.com>
2019-07-21 18:49:43 -0400

    Use CurrentLeagueUseCase

---
+++ app/src/main/java/org/mtg/usecase/CurrentLeagueUseCase.kt
@@ -0,0 +1,33 @@
+package org.mtg.usecase
+
+import io.reactivex.Single
+import org.mtg.model.League
+import org.mtg.model.Settings
+import org.mtg.repository.LeagueRemoteRepository
+import org.mtg.repository.SettingsLocalRepository
+
+class CurrentLeagueUseCase(
+    private val leagueRepo: LeagueRemoteRepository,
+    private val settingsRepo: SettingsLocalRepository
+) {
+
+    data class Result(val leagueId: Long, val leagueName: String)
+
+    fun get() = settingsRepo.get().switchMapSingle { fetchIfNeeded(it) }
+
+    private fun fetchIfNeeded(settings: Settings): Single<Result> {
+        val id = settings.selectedLeagueId
+        val name = settings.selectedLeagueName
+        return if (validLeague(id)) {
+            Single.just(Result(leagueId = id, leagueName = name))
+        } else {
+            leagueRepo.leagues()
+                .map { resultFromLeague(it.firstOrNull() ?: League()) }
+        }
+    }
+
+    private fun validLeague(leagueId: Long) = leagueId > 0
+
+    private fun resultFromLeague(league: League) =
+        Result(leagueId = league.id, leagueName = league.name)
+}
---
+++ app/src/test/java/org/mtg/usecase/CurrentLeagueUseCaseTest.kt
@@ -0,0 +1,85 @@
+package org.mtg.usecase
+
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import io.reactivex.subjects.PublishSubject
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mtg.model.League
+import org.mtg.model.Settings
+import org.mtg.repository.LeagueRemoteRepository
+import org.mtg.repository.SettingsLocalRepository
+
+@RunWith(JUnit4::class)
+class CurrentLeagueUseCaseTest {
+    private companion object {
+        val SETTINGS_WITH_LEAGUE = Settings(selectedLeagueId = 1, selectedLeagueName = "M20")
+        val SETTINGS_WITHOUT_LEAGUE = Settings()
+        val LEAGUE = League(id = 2, name = "War of the Spark")
+    }
+
+    private val leagueSubject = PublishSubject.create<List<League>>()
+    private val settingsSubject = PublishSubject.create<Settings>()
+
+    private val leagueRepo = mock<LeagueRemoteRepository> {
+        on { leagues() } doReturn leagueSubject.firstOrError()
+    }
+    private val settingsRepo = mock<SettingsLocalRepository> {
+        on { get() } doReturn settingsSubject
+    }
+
+    private val useCase = CurrentLeagueUseCase(leagueRepo = leagueRepo, settingsRepo = settingsRepo)
+
+    @Test
+    fun get_fromApi() {
+        val observer = useCase.get().test()
+
+        respondWithEmptySettings()
+        respondWithRemoteLeague()
+
+        observer.assertValues(
+            CurrentLeagueUseCase.Result(
+                leagueId = LEAGUE.id,
+                leagueName = LEAGUE.name
+            )
+        )
+    }
+
+    @Test
+    fun get_fromNowhere() {
+        val observer = useCase.get().test()
+
+        respondWithEmptySettings()
+        respondWithRemoteError()
+
+        observer.assertValues(
+            CurrentLeagueUseCase.Result(
+                leagueId = 0,
+                leagueName = ""
+            )
+        )
+    }
+
+    @Test
+    fun get_fromSettings() {
+        val observer = useCase.get().test()
+
+        respondWithSavedLeague()
+
+        observer.assertValues(
+            CurrentLeagueUseCase.Result(
+                leagueId = SETTINGS_WITH_LEAGUE.selectedLeagueId,
+                leagueName = SETTINGS_WITH_LEAGUE.selectedLeagueName
+            )
+        )
+    }
+
+    private fun respondWithEmptySettings() = settingsSubject.onNext(SETTINGS_WITHOUT_LEAGUE)
+
+    private fun respondWithRemoteError() = leagueSubject.onNext(emptyList())
+
+    private fun respondWithRemoteLeague() = leagueSubject.onNext(listOf(LEAGUE))
+
+    private fun respondWithSavedLeague() = settingsSubject.onNext(SETTINGS_WITH_LEAGUE)
+}
