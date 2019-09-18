d90f0580ef950cd76784d8a9e4b6f0a8fcaab1e4
Nick Cipollo <nick.cipollo@grubhub.com>
2019-09-18 19:42:58 -0400

    Add title of league

--- app/src/main/java/org/mtg/screen/HomeFragment.kt
+++ app/src/main/java/org/mtg/screen/HomeFragment.kt
@@ -6,14 +6,34 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import org.koin.android.ext.android.inject
 import org.mtg.R
+import org.mtg.domain.CurrentLeagueUseCase
 
 abstract class HomeFragment : Fragment() {
+
+    private val currentLeagueUseCase by inject<CurrentLeagueUseCase>()
+    private var leagueDisposable: Disposable? = null
+
+    private val homeActivity get() = activity as? HomeActivity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
+
+    override fun onStart() {
+        super.onStart()
+        observeCurrentLeague()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        leagueDisposable?.dispose()
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) =
         inflater.inflate(R.menu.main_menu, menu)
@@ -31,4 +51,18 @@ abstract class HomeFragment : Fragment() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+
+    private fun observeCurrentLeague() {
+        leagueDisposable?.dispose()
+
+        currentLeagueUseCase.get()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { updateTitle(it.leagueName) }
+            .also { leagueDisposable = it }
+    }
+
+    private fun updateTitle(leagueName: String) {
+        val title = getString(R.string.toolbar_title_format, leagueName)
+        homeActivity?.supportActionBar?.title = title
+    }
 }
